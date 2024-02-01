d6991380be78542a489f5a4a39b98c854f117a41
Vishu20022 <152845116+Vishu20022@users.noreply.github.com>
2024-02-01 16:38:54 +0530

    project
    abym home page design

---
+++ Ios Development/ios-practise.js
@@ -0,0 +1,7 @@
+const Nav_contact=document.querySelector('.Nav-contact')
+
+const contact=()=>{
+    alert('We will contact you soon!')
+}
+
+Nav_contact.addEventListener('click',contact)
---
+++ Ios Development/ios_development.js
@@ -0,0 +1,57 @@
+const contact_us=document.querySelector('#contact_us')
+
+const contacting=()=>{
+    alert('contacting.....')
+}
+contact_us.addEventListener('click',contacting)
+
+// const about=document.querySelector('    #about')
+// function call(){
+//     alert('hello')
+//     console.log('working')
+// }
+// about.addEventListener('click',call)
+
+
+// const about=document.querySelector('.about')
+// const aboutshow=()=>{
+//     if(about.style.display==='none'){
+//         about.style.display==='block'
+//     }
+//     else{
+//         about.style.display==='none'
+//     }
+// }
+
+// about.addEventListener('click',aboutshow)
+
+// const about=document.querySelector('ul')
+// function showabout(){
+//     if(about.style.display==='none'){
+//         about.style.display==="block"
+//     }else{
+//         about.style.display==='none'
+//     }
+// }
+// const about_btn=document.querySelector('.about-btn')
+// const about_list=document.querySelector('ul')
+
+// function showAbout(){
+//     if(about_list.style.display==='none')
+//     {
+//         about_list.style.display==='block'
+//     }else{
+//         about_list.style.display==='none'
+//     }
+// }
+// about_btn.addEventListener('click',showAbout)
+
+// const about_main=document.querySelector('.about-main')
+
+// about_main.addEventListener('click',()=>{
+//     if(about_main.style.display=='none'){
+//         about_main.style.display=='block'
+//     }else{
+//         about_main.style.display=='none'
+//     }
+// })
